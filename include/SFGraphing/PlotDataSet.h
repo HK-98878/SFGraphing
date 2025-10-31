ab0ad3d279f5b5e2d0886bb8f864147024ed120d
Han Se Kendall <hskendall2005@outlook.com>
2025-10-31 13:49:33 +0000

    Type functionality extension
    PlotDataSet can now be constructed from any vector, and will attempt
     to convert to a float type during initialisation
    
    A new function MultiPlotDatSet now exists, allowing you to construct
     multiple sets simultaneously, either from one or many inputs (x), to
     many outputs (y)

--- include/SFGraphing/PlotDataSet.h
+++ include/SFGraphing/PlotDataSet.h
@@ -13,128 +13,200 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
 
+namespace csrc {
+enum PlottingType { POINTS, LINE, BARS };
 
-namespace csrc
-{
-enum PlottingType
-{
-    POINTS,
-    LINE,
-    BARS
+template <typename T, typename = void>
+struct is_iterable_test : std::false_type {};
+template <typename T>
+struct is_iterable_test<
+  T, std::void_t<decltype(std::begin(std::declval<T>())), decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+template <typename T>
+constexpr bool is_iterable = is_iterable_test<T>::value;
+
+class PlotDataSet {
+  private:
+  std::vector<float> _yValues;
+  std::vector<float> _xValues;
+  sf::Color _color;
+  std::string _label;
+  PlottingType _pType;
+
+  public:
+  /**
+   * Create empty data
+   */
+  PlotDataSet();
+
+  /**
+   * Constructor for empty data, but initializing other properties
+   * @param color dataset color
+   * @param label dataset label
+   * @param type dataset type
+   */
+  PlotDataSet(const sf::Color& color, const std::string& label, const PlottingType& type);
+
+  /**
+   * Dataset constructor
+   * @param xValues vector of doubles, representing x axis
+   * @param yValues vector of doubles, representing y axis
+   * @param color   color of the bars/points/line
+   * @param label   data label
+   * @param type    bars/points/line
+   */
+  template <class T_x, class T_y>
+  PlotDataSet(const std::vector<T_x>& xValues, const std::vector<T_y>& yValues,
+              const sf::Color& color, const std::string& label, const PlottingType& type)
+      : _color(color), _label(label), _pType(type) {
+    _xValues = std::vector<float>(xValues.begin(), xValues.end());
+    _yValues = std::vector<float>(yValues.begin(), yValues.end());
+
+    if (_xValues.size() != _yValues.size()) {
+      std::cerr << "Incompatible data sizes" << std::endl;
+      exit(1);
+    }
+  }
+
+  template <class T_x, class T_y>
+  static std::vector<PlotDataSet> MultiPlotDatSet(const std::vector<T_x>& xValues,
+                                                  const std::vector<T_y>& yValues,
+                                                  const sf::Color& color, const PlottingType& type,
+                                                  const std::vector<std::string>& labels = {}) {
+    if constexpr (!is_iterable<T_y> && !is_iterable<T_x>) {  // Only one dataset
+      return std::vector<PlotDataSet>{
+        PlotDataSet(xValues, yValues, color, labels.empty() ? "" : labels[0], type)};
+    }
+    if constexpr (is_iterable<T_y> && !is_iterable<T_x>) {  // Several Y values for each X value
+      size_t sz = xValues.size();
+      size_t internal_sz = yValues[0].size();
+      if (sz == 0)
+        throw std::runtime_error("Empty y value data");
+
+      size_t labels_sz = labels.size();
+      std::vector<PlotDataSet> out(sz);
+
+      std::vector<std::vector<float>> data = std::vector<std::vector<float>>(internal_sz);
+      for (size_t i = 0; i < internal_sz; ++i) {
+        data[i] = std::vector<float>(sz);
+        for (size_t j = 0; j < sz; ++j) {
+          data[i][j] = yValues[j][i];
+        }
+      }
+
+      for (int i = 0; i < internal_sz; ++i) {
+        out[i] = PlotDataSet(xValues, data[i], color, i < labels_sz ? labels[i] : "", type);
+      }
+      return out;
+    }
+    if constexpr (is_iterable<T_x> && is_iterable<T_y>) {  // Several X and Y values
+      size_t sz = xValues.size();
+      size_t internal_sz = yValues[0].size();
+      if (yValues.size() != sz) {
+        throw std::runtime_error("Mismatching dataset sizes for mutliplot, set dimension of X"
+                                 "must be one, or match set dimension of Y");
+      }
+
+      int labels_sz = labels.size();
+      std::vector<PlotDataSet> out(sz);
+
+      std::vector<std::vector<float>> xdata = std::vector<std::vector<float>>(internal_sz);
+      std::vector<std::vector<float>> ydata = std::vector<std::vector<float>>(internal_sz);
+      for (size_t i = 0; i < internal_sz; ++i) {
+        xdata[i] = std::vector<float>(sz);
+        ydata[i] = std::vector<float>(sz);
+        for (size_t j = 0; j < sz; ++j) {
+          xdata[i][j] = xValues[j][i];
+          ydata[i][j] = yValues[j][i];
+        }
+      }
+
+      for (int i = 0; i < sz; ++i) {
+        out[i] = PlotDataSet(xdata[i], ydata[i], color, i < labels_sz ? labels[i] : "", type);
+      }
+      return out;
+    }
+    return {};
+  }
+
+  /**
+   * _xValues Getter
+   * @return _xValues
+   */
+  std::vector<float> GetXValues() const;
+
+  /**
+   * _yValues Getter
+   * @return _yValues
+   */
+  std::vector<float> GetYValues() const;
+
+  /**
+   * Get element count (_xValues ad _yValues always the same size)
+   * @return _xValues.size()
+   */
+  size_t GetDataLength() const;
+
+  /**
+   * Get a specific value
+   * @param  i index
+   * @return   value pair at i
+   */
+  sf::Vector2f GetDataValue(const size_t& i) const;
+
+  /**
+   * Setter for a certain value, by index
+   * @param i         index
+   * @param valuePair new x and y values
+   */
+  void SetDataValue(const size_t& i, const sf::Vector2f& valuePair);
+
+  /**
+   * _color Getter
+   * @return _color
+   */
+  sf::Color GetColor() const;
+
+  /**
+   * _color Setter
+   * @param color _color
+   */
+  void SetColor(const sf::Color& color);
+
+  /**
+   * _label Getter
+   * @return _label
+   */
+  std::string GetLabel() const;
+
+  /**
+   * _label Setter
+   * @param lab new _label
+   */
+  void SetLabel(const std::string& lab);
+
+  /**
+   * _plotType Getter
+   * @return _plotType
+   */
+  PlottingType GetPlottingType() const;
+
+  /**
+   * _plotType Setter
+   * @param type new _plotType
+   */
+  void SetPlottingType(const PlottingType& type);
+
+  /**
+   * Function for adding a pair of values to the dataset
+   * @param pair the pair to be added
+   */
+  void PushPair(const sf::Vector2f& pair);
+
+  /**
+   * Function for erasing a pair of values to the dataset
+   * @param number of elements to erase
+   */
+  void PopFront(const size_t n = 1);
 };
-
-class PlotDataSet
-{
-private:
-    std::vector<float> _yValues;
-    std::vector<float> _xValues;
-    sf::Color _color;
-    std::string _label;
-    PlottingType _pType;
-public:
-
-    /**
-     * Create empty data
-     */
-    PlotDataSet();
-
-    /**
-     * Constructor for empty data, but initializing other properties
-     * @param color dataset color
-     * @param label dataset label
-     * @param type dataset type
-     */
-    PlotDataSet(const sf::Color& color, const std::string& label, const PlottingType& type);
-
-    /**
-     * Dataset constructor
-     * @param xValues vector of doubles, representing x axis
-     * @param yValues vector of doubles, representing y axis
-     * @param color   color of the bars/points/line
-     * @param label   data label
-     * @param type    bars/points/line
-     */
-    PlotDataSet(const std::vector<float>& xValues, const std::vector<float>& yValues, const sf::Color& color, const std::string& label,
-                const PlottingType& type);
-
-    /**
-     * _xValues Getter
-     * @return _xValues
-     */
-    std::vector<float> GetXValues() const;
-
-    /**
-     * _yValues Getter
-     * @return _yValues
-     */
-    std::vector<float> GetYValues() const;
-
-    /**
-     * Get element count (_xValues ad _yValues always the same size)
-     * @return _xValues.size()
-     */
-    size_t GetDataLength() const;
-
-    /**
-     * Get a specific value
-     * @param  i index
-     * @return   value pair at i
-     */
-    sf::Vector2f GetDataValue(const size_t& i) const;
-
-    /**
-     * Setter for a certain value, by index
-     * @param i         index
-     * @param valuePair new x and y values
-     */
-    void SetDataValue(const size_t& i, const sf::Vector2f& valuePair);
-
-    /**
-     * _color Getter
-     * @return _color
-     */
-    sf::Color GetColor() const;
-
-    /**
-     * _color Setter
-     * @param color _color
-     */
-    void SetColor(const sf::Color& color);
-
-    /**
-     * _label Getter
-     * @return _label
-     */
-    std::string GetLabel() const;
-
-    /**
-     * _label Setter
-     * @param lab new _label
-     */
-    void SetLabel(const std::string& lab);
-
-    /**
-     * _plotType Getter
-     * @return _plotType
-     */
-    PlottingType GetPlottingType() const;
-
-    /**
-     * _plotType Setter
-     * @param type new _plotType
-     */
-    void SetPlottingType(const PlottingType& type);
-
-    /**
-     * Function for adding a pair of values to the dataset
-     * @param pair the pair to be added
-     */
-    void PushPair(const sf::Vector2f& pair);
-
-    /**
-     * Function for erasing a pair of values to the dataset
-     * @param number of elements to erase
-     */
-    void PopFront(const size_t n = 1);
-};
-}
+}  // namespace csrc
--- include/SFGraphing/SFPlot.h
+++ include/SFGraphing/SFPlot.h
@@ -8,107 +8,102 @@
  * @license MIT-License
  */
 #include "PlotDataSet.h"
-#include <algorithm>
 #include <cmath>
-#include <iomanip>
-#include <sstream>
 
-namespace csrc
-{
-class SFPlot : public sf::Drawable
-{
-private:
-    sf::Vector2f _origin;
-    sf::Vector2f _dimension;
+namespace csrc {
+class SFPlot : public sf::Drawable {
+  private:
+  sf::Vector2f _origin;
+  sf::Vector2f _dimension;
 
-    float _margin;
+  float _margin;
 
-    std::vector<PlotDataSet> _plotDataSets;
+  std::vector<PlotDataSet> _plotDataSets;
 
-    sf::Font _font;
+  sf::Font _font;
 
-    std::string _xAxisLabel;
-    std::string _yAxisLabel;
+  std::string _xAxisLabel;
+  std::string _yAxisLabel;
 
-    sf::Color _axesColor;
-    sf::Vector2f _xCoordBounds;
-    sf::Vector2f _yCoordBounds;
-    sf::Vector2f _CoordSteps;
+  sf::Color _axesColor;
+  sf::Vector2f _xCoordBounds;
+  sf::Vector2f _yCoordBounds;
+  sf::Vector2f _CoordSteps;
 
-    sf::VertexArray _axesVertexArray;
-    sf::VertexArray _axesIndicatorVertexArray;
+  sf::VertexArray _axesVertexArray;
+  sf::VertexArray _axesIndicatorVertexArray;
 
-    //data lines, all text elements
-    std::vector<sf::VertexArray> _dataSetsVertexArrays;
-    std::vector<sf::Text> _textElementArray;
+  // data lines, all text elements
+  std::vector<sf::VertexArray> _dataSetsVertexArrays;
+  std::vector<sf::Text> _textElementArray;
 
-    /**
-     * SFML Function override
-     * @param target sf::RenderTarget
-     * @param states sf::RenderStates
-     */
-    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+  /**
+   * SFML Function override
+   * @param target sf::RenderTarget
+   * @param states sf::RenderStates
+   */
+  void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
-    /**
-     * Function for converting between plot coordinates and window coordinates
-     * @param  loc The position to convert
-     * @return              Converted Position
-     */
-    sf::Vector2f CoordToWindowPosition(const sf::Vector2f& loc);
+  /**
+   * Function for converting between plot coordinates and window coordinates
+   * @param  loc The position to convert
+   * @return              Converted Position
+   */
+  sf::Vector2f CoordToWindowPosition(const sf::Vector2f& loc);
 
-public:
+  public:
+  /**
+   * Creates a string representation of a double with 'precision' number of digits after comma
+   * @param  d         double to convert
+   * @param  precision number of digits after comma
+   * @return           String representation
+   */
+  std::string ToString(const double& d, const size_t& precision = 2);
 
-    /**
-     * Creates a string representation of a double with 'precision' number of digits after comma
-     * @param  d         double to convert
-     * @param  precision number of digits after comma
-     * @return           String representation
-     */
-    std::string ToString(const double& d, const size_t& precision = 2);
+  /**
+   * Constructor using essential values
+   * @param position  top left position of plot
+   * @param dimension width and height of plot
+   * @param margin    distance of axes from the borders of the plot
+   * @param font      sf::Font reference
+   * @param xLabel    label of x-axis
+   * @param yLabel    label of y-axis
+   */
+  SFPlot(const sf::Vector2f& position, const sf::Vector2f& dimension, const float& margin,
+         const sf::Font& font, const std::string& xLabel, const std::string& yLabel);
 
-    /**
-     * Constructor using essential values
-     * @param position  top left position of plot
-     * @param dimension width and height of plot
-     * @param margin    distance of axes from the borders of the plot
-     * @param font      sf::Font reference
-     * @param xLabel    label of x-axis
-     * @param yLabel    label of y-axis
-     */
-    SFPlot(const sf::Vector2f& position, const sf::Vector2f& dimension, const float& margin, const sf::Font& font, const std::string& xLabel,
-           const std::string& yLabel);
+  /**
+   * Function for automatically deducing axes labeling
+   */
+  void SetupAxes();
 
-    /**
-     * Function for automatically deducing axes labeling
-     */
-    void SetupAxes();
+  /**
+   * Function for setting axes labeling manually
+   * @param xmin      lower x bound
+   * @param xmax      higher x bound
+   * @param ymin      lower y bound
+   * @param ymax      higher y bound
+   * @param xstep     x step size
+   * @param ystep     y step size
+   * @param axesColor color of axes
+   */
+  void SetupAxes(const float& xmin, const float& xmax, const float& ymin, const float& ymax,
+                 const float& xstep, const float& ystep, const sf::Color& axesColor);
 
-    /**
-     * Function for setting axes labeling manually
-     * @param xmin      lower x bound
-     * @param xmax      higher x bound
-     * @param ymin      lower y bound
-     * @param ymax      higher y bound
-     * @param xstep     x step size
-     * @param ystep     y step size
-     * @param axesColor color of axes
-     */
-    void SetupAxes(const float& xmin, const float& xmax, const float& ymin, const float& ymax, const float& xstep, const float& ystep, const sf::Color& axesColor);
+  /**
+   * Function for adding a dataset to the plot
+   * @param set the set to be added
+   */
+  void AddDataSet(const PlotDataSet& data_set);
 
-    /**
-     * Function for adding a dataset to the plot
-     * @param set the set to be added
-     */
-    void AddDataSet(const PlotDataSet& data_set);
+  /**
+   * Generate SFML VertexArrays from Datasets
+   */
+  void GenerateVertices();
 
-    /**
-     * Generate SFML VertexArrays from Datasets
-     */
-    void GenerateVertices();
-
-    /**
-     * Clear SFML VertexArrays
-     */
-    void ClearVertices();
+  /**
+   * Clear SFML VertexArrays
+   */
+  void ClearVertices();
 };
-}
+}  // namespace csrc
